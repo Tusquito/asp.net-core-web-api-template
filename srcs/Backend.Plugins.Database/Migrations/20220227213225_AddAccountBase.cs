c2520fd9436529d8882a1b9e3ec4410a8b568938
Hugo Pagniez <hugo.pagniez@gmail.com>
2022-02-27 23:11:55 +0100

    add migration

---
+++ srcs/Backend.Plugins.Database/Migrations/20220227213225_AddAccountBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Plugins.Database.Migrations
+{
+    public partial class AddAccountBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "account_scheme");
+
+            migrationBuilder.CreateTable(
+                name: "account",
+                schema: "account_scheme",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "text", nullable: false),
+                    Ip = table.Column<string>(type: "text", nullable: false),
+                    AuthorityType = table.Column<int>(type: "integer", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_account", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "account",
+                schema: "account_scheme");
+        }
+    }
+}
